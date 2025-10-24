65176d92e4199fd5210ab7757ac0d63f82afe21d
Juliana82 <julianagomes1703@gmail.com>
2025-10-24 15:46:17 -0300

    Funcionalidade Login e cadastro adicionadas

---
+++ src/app/cadastro/page.tsx
@@ -0,0 +1,8 @@
+import {RegisterForm} from "@/app/components/register/form";
+
+export default function Register() {
+  return <RegisterForm />;
+}
+
+
+//import do register form para poder usar a rota
